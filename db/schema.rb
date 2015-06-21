440d837e97be00ffc000d3001072fe2f2a215d5f
harry gao <foxgaocn@gmail.com>
2015-06-21 17:52:03 +1000

    started send

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150614042730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
